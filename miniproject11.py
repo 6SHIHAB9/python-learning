1afb14de8fc23b4eb8d6cab57cced460a99390eb
6SHIHAB9 <shihabg64@gmail.com>
2026-01-31 13:27:27 +0530

    11

---
+++ miniproject11.py
@@ -0,0 +1,7 @@
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it to live in {location}")
+
+
+greet_with("Shihab","India")
+greet_with(location="India",name="Shihab")
