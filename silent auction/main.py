a66f460be23703376527133675d9dfa121f9ce67
6SHIHAB9 <shihabg64@gmail.com>
2026-02-01 17:07:38 +0530

    silen auction

---
+++ silent auction/main.py
@@ -0,0 +1,19 @@
+from art import logo
+print(logo)
+bidders = {}
+
+while True:
+    name = input("What's your name?: ")
+    bid = int(input("What's your bid?: $"))
+    bidders[name] = bid
+    flow = input("Are there any other bidders? Type \"yes\" or \"no\"\n")
+
+    if flow == "no":
+        highest_bid = 0
+        winner = ""
+        for key in bidders:
+            if bidders[key] > highest_bid:
+                highest_bid = bidders[key]
+                winner = key
+        print(f"The winner is {winner} with a bid of ${highest_bid} ")
+        break
