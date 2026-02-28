377e5d34c15f8b80c3c2285c7a4858a7b196b4f5
6SHIHAB9 <shihabg64@gmail.com>
2026-02-28 23:02:49 +0530

    16

---
+++ miniproject16.py
@@ -0,0 +1,46 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    jumps = 0
+    it = 0
+    pos = 1
+    while pos != len(c): 
+        if it + 2 == len(c):
+            jumps += 1
+            pos += 1
+            it += 1
+        elif c[it + 1] == 0 and c[it + 2] == 0:
+            jumps += 1
+            pos += 2
+            it += 2
+        elif c[it + 1] == 0 and c[it + 2] != 0:
+            jumps += 1
+            pos += 1
+            it += 1
+        elif c[it + 1] != 0 and c[it + 2] == 0:
+            jumps += 1
+            pos += 2
+            it += 2
+    return jumps
+
+            
+            
+        
+        
+
+
+n = 7
+
+c = [0,0,0,1,0,0]
+print(jumpingOnClouds(c))
