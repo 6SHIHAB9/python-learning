eab3c19031d128afa85b146bbcca0dce7e153583
6SHIHAB9 <shihabg64@gmail.com>
2026-04-01 19:06:32 +0530

    commit

--- Quiz OOPs/quiz_brain.py
+++ Quiz OOPs/quiz_brain.py
@@ -5,4 +5,5 @@ class QuizBrain:
 
     def next_question(self):
         question = self.question_list[self.question_no]
+        self.question_no += 1
         answer = input(f"Q.{self.question_no}: {question.text} (True/False): ")
