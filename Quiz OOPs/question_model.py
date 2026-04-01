d92c169935d42c93dbc552b54ef266a6ee013feb
6SHIHAB9 <shihabg64@gmail.com>
2026-04-01 19:06:41 +0530

    commit

--- Quiz OOPs/question_model.py
+++ Quiz OOPs/question_model.py
@@ -1,9 +1,6 @@
 class Question:
+    
     def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
     
-new_q = Question("Adsasdas", "False")
-
-
-print(new_q.text)
